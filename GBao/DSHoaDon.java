9e352d76518faba03fc4dc4a8fa6315a72a57e34
Blaser69 <quachgiabao2341@gmail.com>
2023-11-01 20:10:45 +0700

    GBao2

--- GBao/DSHoaDon.java
+++ GBao/DSHoaDon.java
@@ -2,37 +2,50 @@ import java.util.Scanner;
 
 public class DSHoaDon {
     private HoaDon[] danhSach;
-    private int soLuong;
 
-    public DSHoaDon(int soLuong) {
-        this.soLuong = 0;
-        danhSach = new HoaDon[soLuong];
+    public DSHoaDon() {
+        danhSach = new HoaDon[0];
     }
 
     public void them(HoaDon hd) {
-        if (soLuong < danhSach.length) {
-            danhSach[soLuong] = hd;
-            soLuong++;
+        HoaDon[] newDanhSach = new HoaDon[danhSach.length + 1];
+        for (int i = 0; i < danhSach.length; i++) {
+            newDanhSach[i] = danhSach[i];
         }
+        newDanhSach[danhSach.length] = hd;
+        danhSach = newDanhSach;
     }
 
     public void xoa(int viTri) {
-        if (viTri >= 0 && viTri < soLuong) {
-            for (int i = viTri; i < soLuong - 1; i++) {
-                danhSach[i] = danhSach[i + 1];
+        if (viTri >= 0 && viTri < danhSach.length) {
+            HoaDon[] newDanhSach = new HoaDon[danhSach.length - 1];
+            for (int i = 0; i < viTri; i++) {
+                newDanhSach[i] = danhSach[i];
             }
-            soLuong--;
+            for (int i = viTri; i < danhSach.length - 1; i++) {
+                newDanhSach[i] = danhSach[i + 1];
+            }
+            danhSach = newDanhSach;
         }
     }
 
     public void sua(int viTri, HoaDon hd) {
-        if (viTri >= 0 && viTri < soLuong) {
+        if (viTri >= 0 && viTri < danhSach.length) {
             danhSach[viTri] = hd;
         }
     }
 
+    public HoaDon timKiemMaNV(String maNhanVien) {
+        for (int i = 0; i < danhSach.length; i++) {
+            if (danhSach[i] != null && danhSach[i].getMaNhanVien().equals(maNhanVien)) {
+                return danhSach[i];
+            }
+        }
+        return null;
+    }
+
     public HoaDon timKiem(String maHoaDon) {
-        for (int i = 0; i < soLuong; i++) {
+        for (int i = 0; i < danhSach.length; i++) {
             if (danhSach[i] != null && danhSach[i].getMaHoaDon().equals(maHoaDon)) {
                 return danhSach[i];
             }
@@ -41,69 +54,83 @@ public class DSHoaDon {
     }
 
     public void xuatDanhSach() {
-        for (int i = 0; i < soLuong; i++) {
+        for (int i = 0; i < danhSach.length; i++) {
             if (danhSach[i] != null) {
                 danhSach[i].xuat();
             }
         }
     }
+
     
     public void quanLyDanhSach() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("1. Them hoa don");
-            System.out.println("2. Xoa hoa don");
-            System.out.println("3. Sua thong tin hoa don");
-            System.out.println("4. Tim kiem hoa don");
-            System.out.println("5. Xuat danh sach hoa don");
-            System.out.println("6. Thoat");
-            System.out.print("Nhap lua chon cua ban: ");
-            int luaChon = scanner.nextInt();
-            switch (luaChon) {
-                case 1:
-                    HoaDon hd = new HoaDon();
-                    hd.nhap();
-                    them(hd);
-                    break;
-                case 2:
-                    System.out.println("Nhap ma hoa don can xoa: ");
-                    String maHD = scanner.next();
-                    for (int i = 0; i < soLuong; i++) {
-                        if (danhSach[i] != null && danhSach[i].getMaHoaDon().equals(maHD)) {
-                            xoa(i);
-                        }
-                    }
-                    break;
-                case 3:
-                    System.out.println("Nhap ma hoa don can sua: ");
-                    String ma = scanner.next();
-                        for (int i = 0; i < soLuong; i++) {
-                            if (danhSach[i] != null && danhSach[i].getMaHoaDon().equals(ma)) {
-                                        HoaDon suaHoaDon = new HoaDon();
-                                        suaHoaDon.nhap();
-                                        sua(i, suaHoaDon);
-                            }
-                        }
-                    break;
-                case 4:
-                    System.out.println("Nhap ma hoa don can tim: ");
-                    String n = scanner.next();
-                    HoaDon timkiemHD = timKiem(n);
-                    if (timkiemHD != null) {
-                        timkiemHD.xuat();
-                    } else {
-                        System.out.println("Khong tim thay hoa don co ma: " + n);
-                    }
-                    break;
-                case 5:
-                    xuatDanhSach();
-                    break;
-                case 6:
-                    return;
-                default:
-                    System.out.println("Lua chon khong hop le. Vui long nhap lai.");
-                    break;
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+        System.out.println("1. Them hoa don");
+        System.out.println("2. Xoa hoa don");
+        System.out.println("3. Sua thong tin hoa don");
+        System.out.println("4. Tim kiem hoa don bang ma nhan vien");
+        System.out.println("5. Tim kiem ma hoa don");
+        System.out.println("6. Xuat danh sach hoa don");
+        System.out.println("7. Thoat");
+        System.out.print("Nhap lua chon cua ban: ");
+        int luaChon = scanner.nextInt();
+        switch (luaChon) {
+            case 1:
+                HoaDon hd = new HoaDon();
+                hd.nhap();
+                them(hd);
+                break;
+            case 2:
+                System.out.println("Nhap ma hoa don can xoa: ");
+                String maHD = scanner.next();
+                for (int i = 0; i < danhSach.length; i++) {
+                    if (danhSach[i] != null && danhSach[i].getMaHoaDon().equals(maHD)) {
+                        xoa(i);
+                        break;
                     }
                 }
+                break;
+            case 3:
+                System.out.println("Nhap ma hoa don can sua: ");
+                String ma = scanner.next();
+                for (int i = 0; i < danhSach.length; i++) {
+                    if (danhSach[i] != null && danhSach[i].getMaHoaDon().equals(ma)) {
+                        HoaDon suaHoaDon = new HoaDon();
+                        suaHoaDon.nhap();
+                        sua(i, suaHoaDon);
+                        break;
+                    }
+                }
+                break;
+            case 4:
+                System.out.println("Nhap ma nhan vien can tim: ");
+                String m = scanner.next();
+                HoaDon timkiemNV = timKiemMaNV(m);
+                if (timkiemNV != null) {
+                    timkiemNV.xuat();
+                } else {
+                    System.out.println("Khong tim thay hoa don co ma nhan vien: " + m);
+                }
+                break;
+            case 5:
+                System.out.println("Nhap ma hoa don can tim: ");
+                String n = scanner.next();
+                HoaDon timkiemHD = timKiem(n);
+                if (timkiemHD != null) {
+                    timkiemHD.xuat();
+                } else {
+                    System.out.println("Khong tim thay hoa don co ma: " + n);
+                }
+                break;
+            case 6:
+                xuatDanhSach();
+                break;
+            case 7:
+                return;
+            default:
+                System.out.println("Lua chon khong hop le. Vui long nhap lai.");
+                break;
             }
         }
+    }
+}
