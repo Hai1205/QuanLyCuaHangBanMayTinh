5e2d52438cfd08e81bf5fe35b1cb3beca0ffeb2f
Nguyễn Hoàng Hải <123158512+Hai1205@users.noreply.github.com>
2023-11-12 17:49:25 +0700

    Delete Bao directory

--- Bao/TestDSKH.java
+++
@@ -1,8 +0,0 @@
-class TestDSKH {
-
-    public static void main(String[] args) {
-        MenuKhachHang ds = new MenuKhachHang();
-        ds.MENU();
-    }
-}
-
--- Bao/TestDSNV.java
+++
@@ -1,7 +0,0 @@
-class TestDSNV {
-
-    public static void main(String[] args) {
-        MenuNhanVien ds = new MenuNhanVien();
-        ds.MENU();
-    }
-}
