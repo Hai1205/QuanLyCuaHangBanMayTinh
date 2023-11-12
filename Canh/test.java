440630cf7183b1f8c457e3f95482c2ac4947c9f6
Nguyễn Hoàng Hải <123158512+Hai1205@users.noreply.github.com>
2023-11-12 17:51:42 +0700

    Delete Canh directory

--- Canh/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        DanhSachSanPham b1 = new DanhSachSanPham();
-        QLDSSP b2 = new QLDSSP(b1);
-        b2.menu();
-    }
-}
